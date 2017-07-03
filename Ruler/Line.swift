134f5a3f836f063058e3215fbd0d1288f57d7ec5
Seliz Kaya <seliz.kaya@volumental.com>
2017-07-03 16:44:12 +0200

    first commit

--- Ruler/Line.swift
+++ Ruler/Line.swift
@@ -7,3 +7,212 @@
 //
 
 import Foundation
+import SceneKit
+
+class Line: SCNNode {
+    enum Edge {
+        case min, max
+    }
+    
+    enum Side: String {
+        case front, back
+        case top, bottom
+        case left, right
+        
+        var axis: SCNVector3.Axis {
+            switch self {
+            case .left, .right: return .x
+            case .top, .bottom: return .y
+            case .front, .back: return .z
+            }
+        }
+        
+        var edge: Edge {
+            switch self {
+            case .back, .bottom, .left: return .min
+            case .front, .top, .right: return .max
+            }
+        }
+    }
+    
+    enum HorizontalAlignment {
+        case left, right, center
+        
+        var anchor: Float {
+            switch self {
+            case .left: return 0
+            case .right: return 1
+            case .center: return 0.5
+            }
+        }
+    }
+    
+    enum VerticalAlignment {
+        case top, bottom, center
+        
+        var anchor: Float {
+            switch self {
+            case .bottom: return 0
+            case .top: return 1
+            case .center: return 0.5
+            }
+        }
+    }
+    
+    let labelMargin = Float(0.01)
+    
+    let lineWidth = CGFloat(0.005)
+    
+    let vertexRadius = CGFloat(0.005)
+    
+    let fontSize = Float(0.025)
+    
+    let minLabelDistanceThreshold = Float(0.01)
+    
+    let minHeightFlatteningThreshold = Float(0.05)
+    
+    let lengthFormatter: NumberFormatter
+    
+    lazy var vertexA: SCNNode = self.makeVertex()
+    lazy var vertexB: SCNNode = self.makeVertex()
+    
+    lazy var lineAB: SCNNode = self.makeLine()
+    
+    lazy var widthLabel: SCNNode = self.makeLabel()
+    
+    //MARK: - Constructors
+    
+    override init() {
+        self.lengthFormatter = NumberFormatter()
+        self.lengthFormatter.numberStyle = .decimal
+        self.lengthFormatter.maximumFractionDigits = 1
+        self.lengthFormatter.multiplier = 100
+        
+        super.init()
+        
+        resizeTo(min: .zero, max: .zero)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    fileprivate func makeNode(with geometry: SCNGeometry) -> SCNNode {
+        for material in geometry.materials {
+            material.lightingModel = .constant
+            material.diffuse.contents = UIColor.white
+            material.isDoubleSided = false
+        }
+        
+        let node = SCNNode(geometry: geometry)
+        self.addChildNode(node)
+        return node
+    }
+    
+    fileprivate func makeVertex() -> SCNNode {
+        let ball = SCNSphere(radius: vertexRadius)
+        return makeNode(with: ball)
+    }
+    
+    fileprivate func makeLine() -> SCNNode {
+        let line = SCNBox(width: lineWidth, height: lineWidth, length: lineWidth, chamferRadius: 0)
+        return makeNode(with: line)
+    }
+    
+    fileprivate func makeLabel() -> SCNNode {
+        
+        let text = SCNText(string: "", extrusionDepth: 0.0)
+        text.font = UIFont.boldSystemFont(ofSize: 1.0)
+        text.flatness = 0.01
+        
+        let node = makeNode(with: text)
+        node.setUniformScale(fontSize)
+        
+        return node
+    }
+    
+    
+    //MARK: - Transformation
+    
+    func move(side: Side, to extent: Float) {
+        var (min, max) = boundingBox
+        switch side.edge {
+        case .min: min.setAxis(side.axis, to: extent)
+        case .max: max.setAxis(side.axis, to: extent)
+        }
+        
+        resizeTo(min: min, max: max)
+    }
+    
+    func resizeTo(min minExtents: SCNVector3, max maxExtents: SCNVector3) {
+        let absMin = SCNVector3(x: min(minExtents.x, maxExtents.x), y: min(minExtents.y, maxExtents.y), z: min(minExtents.z, maxExtents.z))
+        let absMax = SCNVector3(x: max(minExtents.x, maxExtents.x), y: max(minExtents.y, maxExtents.y), z: max(minExtents.z, maxExtents.z))
+        
+        boundingBox = (absMin, absMax)
+        update()
+    }
+    
+    fileprivate func update() {
+        let (minBounds, maxBounds) = boundingBox
+        
+        let size = maxBounds - minBounds
+        
+        assert(size.x >= 0 && size.y >= 0 && size.z >= 0)
+        
+        let A = SCNVector3(x: minBounds.x, y: minBounds.y, z: minBounds.z)
+        let B = SCNVector3(x: maxBounds.x, y: minBounds.y, z: minBounds.z)
+        
+        vertexA.position = A
+        vertexB.position = B
+        
+        updateLine(lineAB, from: A, distance: size.x, axis: .x)
+        
+        updateLabel(widthLabel, distance: size.x, horizontalAlignment: .center, verticalAlignment: .top)
+        widthLabel.position = pointInBounds(at: SCNVector3(x: 0.5, y: 0, z: 1)) + SCNVector3(x: 0, y: 0, z: labelMargin)
+        widthLabel.orientation = SCNQuaternion(radians: -Float.pi / 2, around: .axisX)
+        
+        
+        widthLabel.isHidden = size.x < minLabelDistanceThreshold
+        
+        let horizontalNodes = [
+            vertexA, vertexB,
+            lineAB,
+            ]
+        
+        let flatteningRatio = min(size.y, minHeightFlatteningThreshold) / minHeightFlatteningThreshold
+        for node in horizontalNodes {
+            node.scale = SCNVector3(x: 1, y: flatteningRatio, z: 1)
+        }
+    }
+    
+    fileprivate func updateLine(_ line: SCNNode, from position: SCNVector3, distance: Float, axis: SCNVector3.Axis) {
+        guard let Line = line.geometry as? SCNBox else {
+            fatalError("Tried to update something that is not a line")
+        }
+        
+        let absDistance = CGFloat(abs(distance))
+        let offset = distance * 0.5
+        switch axis {
+        case .x:
+            Line.width = absDistance
+            line.position = position + SCNVector3(x: offset, y: 0, z: 0)
+        case .y:
+            Line.height = absDistance
+            line.position = position + SCNVector3(x: 0, y: offset, z: 0)
+        case .z:
+            Line.length = absDistance
+            line.position = position + SCNVector3(x: 0, y: 0, z: offset)
+        }
+    }
+    
+    
+    fileprivate func updateLabel(_ label: SCNNode, distance distanceInMetres: Float, horizontalAlignment: HorizontalAlignment, verticalAlignment: VerticalAlignment) {
+        guard let text = label.geometry as? SCNText else {
+            fatalError("Tried to update something that is not a label")
+        }
+        
+        text.string = lengthFormatter.string(for: NSNumber(value: distanceInMetres))! + " cm"
+        let textAnchor = text.pointInBounds(at: SCNVector3(x: horizontalAlignment.anchor, y: verticalAlignment.anchor, z: 0))
+        label.pivot = SCNMatrix4(translation: textAnchor)
+    }
+}
--- Ruler/ViewController.swift
+++ Ruler/ViewController.swift
@@ -10,9 +10,72 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
-
+class ViewController: UIViewController, ARSCNViewDelegate, UIGestureRecognizerDelegate {
+    
     @IBOutlet var sceneView: ARSCNView!
+    
+    var panGesture: UIPanGestureRecognizer!
+    
+    
+    var line: Line!
+    var hitTestPlane: SCNNode!
+    var floor: SCNNode!
+    
+    var currentAnchor: ARAnchor?
+    
+    struct RenderingCategory: OptionSet {
+        let rawValue: Int
+        static let reflected = RenderingCategory(rawValue: 1 << 1)
+        static let planes = RenderingCategory(rawValue: 1 << 2)
+    }
+    
+    enum InteractionMode {
+        case waitingForLocation
+        case draggingInitialWidth
+    }
+    
+    var planesShown: Bool {
+        get { return RenderingCategory(rawValue: sceneView.pointOfView!.camera!.categoryBitMask).contains(.planes) }
+        set {
+            var mask = RenderingCategory(rawValue: sceneView.pointOfView!.camera!.categoryBitMask)
+            if newValue == true {
+                mask.formUnion(.planes)
+            } else {
+                mask.subtract(.planes)
+            }
+            sceneView.pointOfView!.camera!.categoryBitMask = mask.rawValue
+        }
+    }
+    
+    var mode: InteractionMode = .waitingForLocation {
+        didSet {
+            switch mode {
+            case .waitingForLocation:
+                
+                line.isHidden = true
+                
+                hitTestPlane.isHidden = true
+                floor.isHidden = true
+                
+                planesShown = true
+                
+            case .draggingInitialWidth:
+                
+                line.isHidden = false
+                
+                floor.isHidden = false
+                
+                hitTestPlane.isHidden = false
+                hitTestPlane.position = .zero
+                hitTestPlane.boundingBox.min = SCNVector3(x: -1000, y: 0, z: -1000)
+                hitTestPlane.boundingBox.max = SCNVector3(x: 1000, y: 0, z: 1000)
+                
+                planesShown = false
+                
+                
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,14 +83,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.antialiasingMode = .multisampling4X
+        sceneView.autoenablesDefaultLighting = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.addGestureRecognizer(panGesture)
+        
+        line = Line()
+        line.isHidden = true
+        sceneView.scene.rootNode.addChildNode(line)
+        
+        hitTestPlane = SCNNode()
+        hitTestPlane.isHidden = true
+        line.addChildNode(hitTestPlane)
+        
+        let floorSurface = SCNFloor()
+        floorSurface.reflectivity = 0.2
+        floorSurface.reflectionFalloffEnd = 0.05
+        floorSurface.reflectionCategoryBitMask = RenderingCategory.reflected.rawValue
+        
+        floorSurface.firstMaterial?.diffuse.contents = UIColor.black
+        floorSurface.firstMaterial?.writesToDepthBuffer = false
+        floorSurface.firstMaterial?.blendMode = .add
+        
+        floor = SCNNode(geometry: floorSurface)
+        floor.isHidden = true
+        
+        line.addChildNode(floor)
+        line.categoryBitMask |= RenderingCategory.reflected.rawValue
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,6 +119,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingSessionConfiguration()
+        configuration.planeDetection = .horizontal
         
         // Run the view's session
         sceneView.session.run(configuration)
@@ -47,21 +132,121 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
 
+    // MARK: - Touch handling
+    
+    @objc dynamic func handlePan(_ gestureRecognizer: UIPanGestureRecognizer) {
+        switch mode {
+        case .waitingForLocation:
+            findStartingLocation(gestureRecognizer)
+        case .draggingInitialWidth:
+            handleInitialWidthDrag(gestureRecognizer)
+        }
+    }
+    
+    // MARK: Drag Gesture handling
+    
+    func findStartingLocation(_ gestureRecognizer: UIPanGestureRecognizer) {
+        switch gestureRecognizer.state {
+        case .began, .changed:
+            let touchPos = gestureRecognizer.location(in: sceneView)
+            
+            let hit = realWorldHit(at: touchPos)
+            if let startPos = hit.position, let plane = hit.planeAnchor {
+                line.position = startPos
+                currentAnchor = plane
+                mode = .draggingInitialWidth
+            }
+        default:
+            break
+        }
+    }
+    
+    func handleInitialWidthDrag(_ gestureRecognizer: UIPanGestureRecognizer) {
+        switch gestureRecognizer.state {
+        case .began:
+            mode = .waitingForLocation
+        case .changed:
+            let touchPos = gestureRecognizer.location(in: sceneView)
+            if let locationInWorld = scenekitHit(at: touchPos, within: hitTestPlane) {
+                let delta = line.position - locationInWorld
+                let distance = delta.length
+                
+                let angleInRadians = atan2(delta.z, delta.x)
+                
+                line.move(side: .right, to: distance)
+                line.rotation = SCNVector4(x: 0, y: 1, z: 0, w: -(angleInRadians + Float.pi))
+            }
+        case .ended, .cancelled:
+            mode = .draggingInitialWidth
+        default:
+            break
+        }
+    }
+    
+    
+    // MARK: - Hit-testing
+    
+    func scenekitHit(at screenPos: CGPoint, within rootNode: SCNNode) -> SCNVector3? {
+        let hits = sceneView.hitTest(screenPos, options: [
+            .boundingBoxOnly: true,
+            .firstFoundOnly: true,
+            .rootNode: rootNode,
+            .ignoreChildNodes: true
+            ])
+        
+        return hits.first?.worldCoordinates
+    }
+    
+    func realWorldHit(at screenPos: CGPoint) -> (position: SCNVector3?, planeAnchor: ARPlaneAnchor?, hitAPlane: Bool) {
+        
+        let planeHitTestResults = sceneView.hitTest(screenPos, types: .existingPlaneUsingExtent)
+        if let result = planeHitTestResults.first {
+            
+            let planeHitTestPosition = SCNVector3.positionFromTransform(result.worldTransform)
+            let planeAnchor = result.anchor
+            
+            return (planeHitTestPosition, planeAnchor as? ARPlaneAnchor, true)
+        }
+        
+        return (nil, nil, false)
+    }
+    
+    
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {
+            return nil
+        }
+        
+        let plane = SCNBox(width: CGFloat(planeAnchor.extent.x),
+                           height: 0.0001,
+                           length: CGFloat(planeAnchor.extent.z), chamferRadius: 0)
+        
+        if let material = plane.firstMaterial {
+            material.lightingModel = .constant
+            material.diffuse.contents = UIColor.red
+            material.transparency = 0.1
+            material.writesToDepthBuffer = false
+        }
+        
+        let node = SCNNode(geometry: plane)
+        node.categoryBitMask = RenderingCategory.planes.rawValue
+        
         return node
     }
-*/
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor, let plane = node.geometry as? SCNBox else {
+            return
+        }
+        
+        plane.width = CGFloat(planeAnchor.extent.x)
+        plane.length = CGFloat(planeAnchor.extent.z)
+        
+        node.pivot = SCNMatrix4(translationByX: -planeAnchor.center.x, y: -planeAnchor.center.y, z: -planeAnchor.center.z)
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
