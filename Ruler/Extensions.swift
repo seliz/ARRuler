8e7bdd904a7e19ee06bc30f7615feb48ff2a145e
Seliz Kaya <seliz.kaya@volumental.com>
2017-07-03 16:33:25 +0200

    new commit

---
+++ Ruler/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  Ruler
+//
+//  Created by Seliz Kaya on 7/3/17.
+//  Copyright © 2017 Seliz Kaya. All rights reserved.
+//
+
+import Foundation
---
+++ Ruler/Line.swift
@@ -0,0 +1,9 @@
+//
+//  Line.swift
+//  Ruler
+//
+//  Created by Seliz Kaya on 7/3/17.
+//  Copyright © 2017 Seliz Kaya. All rights reserved.
+//
+
+import Foundation
